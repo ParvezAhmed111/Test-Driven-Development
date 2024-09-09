986dd324158dc2c56cd3ead9054a6023104adbbd
Sheikh Parvez Ahmed <parvezahmed.0947@gmail.com>
2024-09-09 23:43:10 +0530

    added test case to handle different delimiters between numbers

--- src/test/java/com/tdd/calculator/StringCalculatorTest.java
+++ src/test/java/com/tdd/calculator/StringCalculatorTest.java
@@ -34,5 +34,11 @@ public class StringCalculatorTest {
         assertEquals(6, calculator.add("1\n2,3"));
     }
 
+    @Test
+    void testDifferentDelimiter() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
 }
 
